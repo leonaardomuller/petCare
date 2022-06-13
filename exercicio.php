5d5dbc25407348ddc688424462eedc6323d9df71
leonaardomuller <leonaardo.muller@gmail.com>
2022-06-13 20:25:45 -0300

    styles css

---
+++ exercicio.php
@@ -0,0 +1,22 @@
+<?php
+// Variáveis
+$num1 = 5;
+$num2 = 10;
+
+// Somando
+$resultado = $num1 + $num2;
+
+echo $resultado;
+?>
+
+
+<?php
+// Variáveis para o calculo
+$num1 = 10;
+$num2 = 4;
+
+// Multiplicação
+$resultado = $num1 * $num2;
+
+echo $resultado;
+?>
